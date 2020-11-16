2d9cdf706e266694a092624eeba5a54dbccde056
Rafal Kozlowski <rafal.kozlowski@ultrico.com>
2020-11-16 23:25:20 +0100

    add initial release scripts

---
+++ releaseScripts/changeVersion.sh
@@ -0,0 +1,20 @@
+!#/bin/bash
+
+version=`cat ./version`
+
+major=0
+minor=0
+build=0
+
+# break down the version number into it's components
+regex="([0-9]+).([0-9]+).([0-9]+)"
+if [[ $version =~ $regex ]]; then
+  major="${BASH_REMATCH[1]}"
+  minor="${BASH_REMATCH[2]}"
+  build="${BASH_REMATCH[3]}"
+fi
+
+build=$((build + 1))
+
+echo "${major}.${minor}.${build}"
+
